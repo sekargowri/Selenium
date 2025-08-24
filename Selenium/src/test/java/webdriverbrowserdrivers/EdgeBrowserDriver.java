6b54888fa3c768ce98b91e9573dbfa460d6ba08c
Gowri <Gowri@192.168.1.3>
2025-08-24 06:18:12 +0530

    script to launch edge browser

---
+++ Selenium/src/test/java/webdriverbrowserdrivers/EdgeBrowserDriver.java
@@ -0,0 +1,13 @@
+package webdriverbrowserdrivers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class EdgeBrowserDriver {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new EdgeDriver();
+	}
+
+}
