b710c101de88129b24d2ebbeb499b16c8fe394ff
Gowri <Gowri@192.168.1.3>
2025-08-24 06:16:56 +0530

    script to launch chrome browser

---
+++ Selenium/src/test/java/webdriverbrowserdrivers/ChromeBrowserDriver.java
@@ -0,0 +1,14 @@
+package webdriverbrowserdrivers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeBrowserDriver {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+
+	}
+
+}
